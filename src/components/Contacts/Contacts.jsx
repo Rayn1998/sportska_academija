49901088a02c6177ac81d6a89d1e720ce78b1f78
Yuriy Bodolanov <magikonermad@yandex.ru>
2023-03-17 13:52:16 +0100

    feat: поправил увиденные недочеты, добавил фото

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -6,29 +6,20 @@ const Contacts = () => {
             <form
 				className="contacts__form"
 				name="contact__form"
-				action="https://formsubmit.co/yuriy.bodolanov@gmail.com"
+				// action="https://formsubmit.co/yuriy.bodolanov@gmail.com"
+				action="#"
 				method="POST"
-				// onSubmit={handleSubmit}
 			>
 				<input
 					type="email"
 					name="email"
 					className="contacts__form-input contacts__form-email"
-					// value={formValues.email}
-					// onChange={handleForm}
 					placeholder="Enter your email..."
 					required
 				/>
-				{/* <input
-					type="hidden"
-					name="_next"
-					value="https://bodolanov-vfx.com/#/thank"
-				></input> */}
 				<textarea
 					className="contacts__form-input contacts__form-text"
 					type="message"
-					// value={formValues.message}
-					// onChange={handleForm}
 					placeholder="Write your message..."
 					name="message"
 					required
