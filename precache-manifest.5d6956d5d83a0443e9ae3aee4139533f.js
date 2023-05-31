1d0bcb1954b33c51402de1b0ef60434b837e9851
Yuriy Bodolanov <magikonermad@yandex.ru>
2023-05-31 14:30:12 +0100

    Updates

---
+++ precache-manifest.5d6956d5d83a0443e9ae3aee4139533f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "38bcae043ed62d0b11fc",
+    "url": "/sportska_academija/static/css/main.a5698091.chunk.css"
+  },
+  {
+    "revision": "38bcae043ed62d0b11fc",
+    "url": "/sportska_academija/static/js/main.38bcae04.chunk.js"
+  },
+  {
+    "revision": "5f585773047137d759ec",
+    "url": "/sportska_academija/static/css/1.46b21357.chunk.css"
+  },
+  {
+    "revision": "5f585773047137d759ec",
+    "url": "/sportska_academija/static/js/1.5f585773.chunk.js"
+  },
+  {
+    "revision": "8b41e46145d778273bec",
+    "url": "/sportska_academija/static/js/runtime~main.8b41e461.js"
+  },
+  {
+    "revision": "7a73a8e766b023424bfdbd2c1ef91819",
+    "url": "/sportska_academija/static/media/home_bg.7a73a8e7.png"
+  },
+  {
+    "revision": "42b4625778d86ddb6c46f6fbb1853e87",
+    "url": "/sportska_academija/static/media/x-mark.42b46257.png"
+  },
+  {
+    "revision": "07bee3ee5979bd726107e6d434685b43",
+    "url": "/sportska_academija/static/media/loading.07bee3ee.png"
+  },
+  {
+    "revision": "43bd626e352a934f8530288daf873f1e",
+    "url": "/sportska_academija/static/media/1.43bd626e.png"
+  },
+  {
+    "revision": "18efe57929890fc110b2242a9efb8162",
+    "url": "/sportska_academija/static/media/2.18efe579.jpg"
+  },
+  {
+    "revision": "ba17aeae82a6467345b7727d8c23ce3d",
+    "url": "/sportska_academija/static/media/3.ba17aeae.png"
+  },
+  {
+    "revision": "f4e0c011797b87fda53f45a6e5b83d54",
+    "url": "/sportska_academija/static/media/4.f4e0c011.jpg"
+  },
+  {
+    "revision": "99323485a9f1b4c0788dfee02ce99216",
+    "url": "/sportska_academija/static/media/Heart_with_Pulse.99323485.png"
+  },
+  {
+    "revision": "028d3a8fa1b7c69dff607e134c137c52",
+    "url": "/sportska_academija/static/media/Muscle_Flexing.028d3a8f.png"
+  },
+  {
+    "revision": "bcc6ca7df9e7f1a280854c531393eb23",
+    "url": "/sportska_academija/static/media/Yoga.bcc6ca7d.png"
+  },
+  {
+    "revision": "ef69681139580db83bb9eaab9fd2d758",
+    "url": "/sportska_academija/static/media/crowd.ef696811.png"
+  },
+  {
+    "revision": "6cb6d6662143619718264fc656535b6c",
+    "url": "/sportska_academija/static/media/1.6cb6d666.jpg"
+  },
+  {
+    "revision": "c4d5920512e1bddde054e538ef8f1889",
+    "url": "/sportska_academija/static/media/2.c4d59205.jpg"
+  },
+  {
+    "revision": "870856ede59a15430fb125e5a8f2672b",
+    "url": "/sportska_academija/static/media/3.870856ed.jpg"
+  },
+  {
+    "revision": "1f7bdcc11ff07d36340e86184c6be5ce",
+    "url": "/sportska_academija/static/media/4.1f7bdcc1.jpeg"
+  },
+  {
+    "revision": "895af0d79aebb4ef2279d7f691dc8b92",
+    "url": "/sportska_academija/static/media/BebasNeue-Regular.895af0d7.ttf"
+  },
+  {
+    "revision": "4c6a21695c10c9c77e7676ffa4a389c1",
+    "url": "/sportska_academija/static/media/Close_Icon.4c6a2169.svg"
+  },
+  {
+    "revision": "0dae51d82f7d4fd816fd4a3e4658a3a6",
+    "url": "/sportska_academija/static/media/logo.0dae51d8.jpg"
+  },
+  {
+    "revision": "773c98ade2b0e41c08e0ce13ec511c24",
+    "url": "/sportska_academija/static/media/arrow-up1.773c98ad.png"
+  },
+  {
+    "revision": "f46192d7ac86e19733cf172f6caee3a1",
+    "url": "/sportska_academija/static/media/bg.f46192d7.png"
+  },
+  {
+    "revision": "cda21edd972e1b85361bb307d52ce2a3",
+    "url": "/sportska_academija/index.html"
+  }
+];
